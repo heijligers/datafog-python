1154527b0c5a73e59c034aabd9702fbf6483188d
Sid Mohan <sidmohan@Sids-MacBook-Pro.local>
2024-08-18 08:28:25 -0700

    pre-commit

--- datafog/exceptions.py
+++ datafog/exceptions.py
@@ -1,21 +1,28 @@
 # exceptions.py
 
+
 class DataFogException(Exception):
     """Base exception for DataFog SDK"""
+
     def __init__(self, message: str, status_code: int = None):
         self.message = message
         self.status_code = status_code
         super().__init__(self.message)
 
+
 class BadRequestError(DataFogException):
     """Exception raised for 400 Bad Request errors"""
+
     def __init__(self, message: str):
         super().__init__(message, status_code=400)
 
+
 class UnprocessableEntityError(DataFogException):
     """Exception raised for 422 Unprocessable Entity errors"""
+
     def __init__(self, message: str):
         super().__init__(message, status_code=422)
+
 
 def raise_for_status_code(status_code: int, error_message: str):
     """Raise the appropriate exception based on the status code"""
